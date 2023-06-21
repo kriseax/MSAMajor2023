f995afb952e7b89db332c459d8c9143baa4cb320
Kristofferson Culmer <krcd58@mail.missouri.edu>
2023-06-21 10:59:13 -0500

    Add files via upload

---
+++ flask/MovieApp/schema.sql
@@ -0,0 +1,22 @@
+--actors schema
+CREATE TABLE actors (
+ id INTEGER PRIMARY KEY NOT NULL,
+ movie_id INTEGER NOT NULL,
+ imdb_id VARCHAR(20) NOT NULL,
+ name VARCHAR(255) NOT NULL
+);
+
+--movies schema
+CREATE TABLE movies (
+ id INTEGER PRIMARY KEY NOT NULL,
+ imdb_id VARCHAR(20) NOT NULL,
+ title VARCHAR(255) NOT NULL,
+ director VARCHAR(255) NOT NULL,
+ year INT NOT NULL,
+ rating VARCHAR(8) NOT NULL,
+ genres VARCHAR(255) NOT NULL,
+ runtime INT NOT NULL,
+ country VARCHAR(255) NOT NULL,
+ language VARCHAR(255) NOT NULL,
+ imdb_score NUMERIC NOT NULL,
+ imdb_votes INT NOT NULL,

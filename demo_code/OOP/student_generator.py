df39ae057f0a660d998ea7a265528f48dcd5a134
Kristofferson Culmer <krcd58@mail.missouri.edu>
2023-06-23 10:20:15 -0500

    Add files via upload

--- demo_code/OOP/student_generator.py
+++ demo_code/OOP/student_generator.py
@@ -1,12 +1,41 @@
 import Student
 #Create 2 instances of Student
-student1 = Student.Student("Cyd", "Robertson", "Jounalism", 55, 3.5, "12345")
-student2 = Student.Student("Frank", "Williams", "Soil Sciences", 77, 3.4, "23456")
+list_of_students = []
 
-students = []
+try:
+    #create a file handler
+    file = open("students.csv", "r")
 
-students.append(student1)
-students.append(student2)
+    #create variable to keep track of line in file that we are reading
+    line_number = 0
+    #read file line by line in for loop
+    for line_of_data in file:
+        line_number += 1
+        #skip first line in csv file
+        if line_number == 1:
+            continue
+        
+        #split the line of data at the comma
+        student_data = line_of_data.split(",")
 
-for student in students:
+        #handle errors in data format. line_of_data should have 6 items
+        if len(student_data) != 6:
+            raise Exception(f"There is an error in your data file on line {line_number}")
+        
+        #get student data and create a student object for each student
+        first_name = student_data[0]
+        last_name = student_data[1]
+        major = student_data[2]
+        credit_hours = int(student_data[3])
+        gpa = float(student_data[4])
+        student_id = student_data[5].strip()
+
+        new_student = Student.Student(first_name, last_name, major, credit_hours, gpa, student_id)
+        list_of_students.append(new_student)
+
+except Exception as err:
+    print(err)
+
+
+for student in list_of_students:
     student.print_student_data()
